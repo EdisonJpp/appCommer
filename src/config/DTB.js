53764ab4cd639ceef7934cb99bde4f5a924cd64e
EdsonJpp <edisojp@gmail.com>
2020-06-20 18:34:50 -0400

    arreglando el modelo de usuarios

--- src/config/DTB.js
+++ src/config/DTB.js
@@ -1,7 +1,7 @@
 const sequelize = require('sequelize');
-module.exports  = new sequelize('COMMERCE', 'sa', 'Edisonjp00254', {
-    host : 'localhost',
-    port : '1433',
+module.exports  = new sequelize('DB_A635CF_tobuy', 'DB_A635CF_tobuy_admin', 'edisonjp00254', {
+    host : 'sql5050.site4now.net',
+    // port : '1433',
         dialect: 'mssql',
         dialectOptions: {
           options: {
